b0547c8ec8148e77a06ee049aef122f90ad2b87c
David Kais <chinh4523@gmail.com>
2020-08-03 22:03:47 +0700

    add assets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./assets/images/icons/favicon.png";
+import "./assets/vendor/bootstrap/css/bootstrap.min.css";
+import "./assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css";
+import "./fonts/iconic/css/material-design-iconic-font.min.css";
+import "./assets/fonts/linearicons-v1.0.0/icon-font.min.css";
+import "./assets/vendor/animate/animate.css";
+import "./assets/vendor/css-hamburgers/hamburgers.min.css";
+import "./assets/vendor/animsition/css/animsition.min.css";
+import "./assets/vendor/select2/select2.min.css";
+import "./assets/vendor/daterangepicker/daterangepicker.css";
+import "./assets/vendor/slick/slick.css";
+import "./assets/vendor/MagnificPopup/magnific-popup.cs";
+import "./assets/vendor/perfect-scrollbar/perfect-scrollbar.css";
+import "./assets/css/util.css";
+import "./assets/css/main.css";
+// import axios from "axios";
+class App extends React.Component {
+  state = {products: []}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(products => this.setState({ products }));
+  }
+  render() {
+    return <div>
+      {this.state.products.map(product=>(
+        <p key={product._id}>{product.name}</p>
+      ))}
+    </div>;
+  }
 }
 
 export default App;
